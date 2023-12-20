b2c30b0270b1dcb0ce851676f6af35e8179c4f1d
Rawan <rssalghamdi@gmail.com>
2023-12-20 12:01:10 +0300

    vvvv

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 (function () {
-  // strict mode enforces stricter parsing and error handling on the code at runtime.
+  strict mode enforces stricter parsing and error handling on the code at runtime.
 'use strict';
 
 var x =
 
 angular.module('NameCalculator', [])
+.controller('NameCalculatorController' , NameCalculatorController);
 
-.controller('NameCalculatorController' , function ($scope) {
-$scope.name = "" ;
-$scope.totalValue = 0 ;
+$scope.name = "RRR" ;
 
-$scope.displayNumeric= function () {
-var totalNameValue = calculateNumericForString ($scope.name)  ; // get the total value
-$scope.totalValue = totalNameValue;
+// NameCalculatorController.$inject = ['$scope', '$filter' ];
+function NameCalculatorController ($scope, $filter) {
+
+    $scope.name = "RRR" ;
+
+$scope.upper = function () {
+  var upCase = $filter('uppercase');
+ $scope.name = upCase($scope.name);
 
 };
 
-function calculateNumericForString (string) {
-var totalStringValue = 0 ;
-for (var i = 0 ; i < string.length ; i ++){
-
-  totalStringValue += string.charCodeAt(i);
-
-}
- return totalStringValue;
 
 }
 
-});
-
-
+// function AnnotateMe (name, job, blah){
+//
+//  return "Blah!"
+// }
 })();
